a521d497497a72454f943de3d46065ee845e7e63
loidd8 <loidd8@gmail.com>
2022-05-12 00:14:40 -0500

    Added security token in .env file

--- src/index.py
+++ src/index.py
@@ -1,11 +1,17 @@
 import discord
+import os
+import dotenv
+
 from discord.ext import commands
+from dotenv import load_dotenv
+
+
 
 bot = commands.Bot(command_prefix='-', description="ver1.0")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def uwu(ctx):
+    await ctx.send('uwu')
 
 #event
 @bot.event
@@ -13,4 +19,7 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Streaming(name="Discord", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", details="Madagascar"))
     print('successfully connected')
 
-bot.run('TOKEN')
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot.run(TOKEN)
