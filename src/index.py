5ed3f1163f41613950b1753044500a2e57611ad8
110nitro <lolnitro08@gmail.com>
2022-07-10 07:09:12 -0500

    Hello World

--- src/index.py
+++ src/index.py
@@ -1,6 +1,12 @@
 import discord
+
 import os
 import dotenv
+
+import datetime
+
+from urllib import parse, request
+import re
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -10,8 +16,37 @@ from dotenv import load_dotenv
 bot = commands.Bot(command_prefix='-', description="ver1.0")
 
 @bot.command()
-async def uwu(ctx):
-    await ctx.send('uwu')
+async def Hello(ctx):
+    await ctx.send('World')
+
+@bot.command()
+async def embed(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description=f"ewe for {ctx.guild.name}", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.set_thumbnail(url=f"https://cdn.discordapp.com/icons/{ctx.guild.id}/{ctx.guild.icon}.webp?size=96")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def search(ctx, * ,search):
+    busqueda = parse.urlencode({'search_query':search})
+    contenido = request.urlopen('http://www.youtube.com/results?' + busqueda)
+    resultados = re.findall(r"watch\?v=(\S{11})", contenido.read().decode())
+    #print(resultados)
+    await ctx.send('https://www.youtube.com/watch?v=' + resultados[0])
+
+@bot.command()
+async def play(ctx, url : str):
+    vc = discord.utils.get(ctx.guild.voice_channels, name='Sala I')
+    voz = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    await vc.connect()
+
+
+
+
+
+
+
+
+
 
 #event
 @bot.event
